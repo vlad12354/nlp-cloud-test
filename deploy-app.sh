ce6bd9f00a187bfc1c83a2e1a9c273652879c1fa
Vladimir <vlad12354@gmail.com>
2018-03-22 17:35:42 +0100

    Test and deployment scripts with clean up repo

---
+++ deploy-app.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+gcloud app deploy app.yaml --project heroic-calculus-198812
---
+++ test-local-app.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "Check the website with the left-button tool (8080 port...)"
+dev_appserver.py $PWD

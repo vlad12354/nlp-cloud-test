99819011d94a20df371248dd1e09aafae5a47e2e
stekanius <stekanius@hotmail.com>
2018-03-22 17:52:16 +0100

    similarity output v2.0

--- similarity_output.py
+++ similarity_output.py
@@ -1,34 +1,33 @@
 """
-XXX
+similarity function
 """
 
-
-import numpy as np
-from difflib import SequenceMatcher
-import os
 import pandas as pd
 from fuzzywuzzy import fuzz
 
 
-df = pd.read_csv('glossary.csv')
-
 # list = RECEIVED BY ANGEL
-list = [ 'Interest Rata']
 
-size = df.shape[0]
+def match_and_output(list):
 
-for term in list:
-    max = 0
-    for j in range(0,size):
-        gl_term = df['term'].ix[j]
-        similarity = fuzz.token_sort_ratio( term, gl_term )
-        if similarity >=max:
-            max = similarity
-            max_ind = j
-    if max>80:
-     print df['definition'].ix[max_ind]
-     print max
+    df = pd.read_csv('glossary.csv')
+    size = df.shape[0]
+    out_dict = {}
+    for term in list:
+        max = 0
+        for j in range(0,size):
+            gl_term = df['term'].ix[j]
+            similarity = fuzz.token_sort_ratio( term, gl_term )
+            if similarity >=max:
+                max = similarity
+                max_ind = j
+        if max>80:
+        print df['definition'].ix[max_ind]
+        print max
+        out_dict[term]= df['definition'].ix[max_ind]
 
+
+    return out_dict
 
 
 

94c01ec1c0817d5d1b3013b92c7574896cfcd4a9
congchen <cong199178@gmail.com>
2018-03-22 16:51:31 +0100

    Add home html with input box.

--- main.py
+++ main.py
@@ -15,20 +15,28 @@
 # [START app]
 import logging
 
-from flask import Flask
-
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def hello():
-    return 'Hello World! 2'
+    return render_template('home.html')
 
+@app.route('/', methods=['POST'])
+def my_form_post():
+    text=request.form['text']
+    processed_text=text.upper()
+    return processed_text
 
 @app.errorhandler(500)
 def server_error(e):
     # Log the error and stacktrace.
     logging.exception('An error occurred during a request.')
     return 'An internal error occurred.', 500
+
+if __name__ == "__main__":
+    app.run()
+
 # [END app]
